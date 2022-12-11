8be2bc3d5e507a76b37898207d7ace43211815e7
DaltonCB <112210162+DaltonCB@users.noreply.github.com>
2022-12-11 15:51:19 -0500

    fix select product button

--- src/MainInterface.java
+++ src/MainInterface.java
@@ -1259,6 +1259,7 @@ public class MainInterface extends JComponent implements Runnable {
                             JOptionPane.showMessageDialog(null,
                                     "New products imported successfully!",
                                     "Import Products", JOptionPane.INFORMATION_MESSAGE);
+                            selectProductButton.setVisible(true);
                         }
 
                     } else {
